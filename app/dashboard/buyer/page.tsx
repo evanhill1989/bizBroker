f3de9131cf45dec45219618313e33c293a3e3eab
Evan Hill <evanhill1989@gmail.com>
2025-02-09 09:01:11 -0500

    buyerdash style

--- app/dashboard/buyer/page.tsx
+++ app/dashboard/buyer/page.tsx
@@ -1,5 +1,6 @@
 import { getExactMatchListings } from "@/app/utils/actions/actions";
 import { requireUser } from "@/app/utils/requireUser";
+import { Card } from "@/components/ui/card";
 import { prisma } from "@/lib/prisma";
 
 
@@ -34,16 +35,27 @@ export default async function BuyerDashboardPage() {
       <p className="text-gray-600 mb-8">
         Complete the steps below to get started!
       </p>
-      <div className="w-[1200px]">
+      <div className="sm:w-[400px] md:w-[600px] lg:w-[1200px]">
         <div className="listings grid grid-flow-col auto-cols-max  overflow-x-auto whitespace-nowrap border p-2">
         {tempMatchingListings.map((listing, index) => (
-          <div key={listing.id} className="listing-item mr-4">
+          <Card key={listing.id} className="mr-4">
+               <div>
+                <div>
+                  <p>icon</p>
+                  <h3>{listing.businessModel}</h3>
+                </div>
+                <div className="like">hu7  
+                  <p>eyeball</p>
+                  <p>heart</p>
+                </div>
+               </div>
             <div className="flex">
               <h2 className="text-lg font-semibold">{index + 1}.{listing.name}</h2>
             </div>
             <p>${listing.price}</p>
-            {/* Add more details as needed */}
-          </div>
+          </Card>
+    
+          
         ))}
       </div>
       </div>
