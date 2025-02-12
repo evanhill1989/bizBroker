d80a4eb33149e05a7859db8a03b936a465de654f
EvanReact <evanhill1989@gmail.com>
2025-02-12 12:30:27 -0500

    removed onboarding/buyers/dead

--- app/dashboard/seller/listings/new/page.tsx
+++ app/dashboard/seller/listings/new/page.tsx
@@ -1,94 +1,94 @@
 "use client";
 
-import { CreateListingAction } from "@/app/utils/actions/actions";
-import { listingSchema } from "@/app/utils/zodSchemas";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { parseWithZod } from "@conform-to/zod";
-import { useActionState } from "react";
-import { useForm } from "@conform-to/react";
-import { SubmitButton } from "@/components/dashboard/SubmitButtons";
+// import { CreateListingAction } from "@/app/utils/actions/actions";
+// import { listingSchema } from "@/app/utils/zodSchemas";
+// import {
+//   Card,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+//   CardContent,
+//   CardFooter,
+// } from "@/components/ui/card";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// import { Textarea } from "@/components/ui/textarea";
+// import { parseWithZod } from "@conform-to/zod";
+// import { useActionState } from "react";
+// import { useForm } from "@conform-to/react";
+// import { SubmitButton } from "@/components/dashboard/SubmitButtons";
 
 export default function NewSiteRoute() {
-  const [lastResult, action] = useActionState(CreateListingAction, undefined);
+//   const [lastResult, action] = useActionState(CreateListingAction, undefined);
 
-  const [form, fields] = useForm({
-    lastResult,
+//   const [form, fields] = useForm({
+//     lastResult,
 
-    onValidate({ formData }) {
-      return parseWithZod(formData, {
-        schema: listingSchema,
-      });
-    },
+//     onValidate({ formData }) {
+//       return parseWithZod(formData, {
+//         schema: listingSchema,
+//       });
+//     },
 
-    shouldValidate: "onBlur",
-    shouldRevalidate: "onInput",
-  });
+//     shouldValidate: "onBlur",
+//     shouldRevalidate: "onInput",
+//   });
 
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <Card className="max-w-[450px]">
-        <CardHeader>
-          <CardTitle>Create Listing</CardTitle>
-          <CardDescription>
-            Create your Listing here. Click button below when finished.
-          </CardDescription>
-        </CardHeader>
-        <form id={form.id} onSubmit={form.onSubmit} action={action}>
-          <CardContent>
-            <div className="flex flex-col gap-y-6">
-              <div className="grid gap-2">
-                <Label>Listing Name</Label>
-                <Input
-                  name={fields.name.name}
-                  key={fields.name.key}
-                  defaultValue={fields.name.initialValue}
-                  placeholder="Listing Name"
-                />
-                <p className="text-red-500 text-sm">{fields.name.errors}</p>
-              </div>
+//   return (
+//     <div className="flex flex-col items-center justify-center">
+//       <Card className="max-w-[450px]">
+//         <CardHeader>
+//           <CardTitle>Create Listing</CardTitle>
+//           <CardDescription>
+//             Create your Listing here. Click button below when finished.
+//           </CardDescription>
+//         </CardHeader>
+//         <form id={form.id} onSubmit={form.onSubmit} action={action}>
+//           <CardContent>
+//             <div className="flex flex-col gap-y-6">
+//               <div className="grid gap-2">
+//                 <Label>Listing Name</Label>
+//                 <Input
+//                   name={fields.name.name}
+//                   key={fields.name.key}
+//                   defaultValue={fields.name.initialValue}
+//                   placeholder="Listing Name"
+//                 />
+//                 <p className="text-red-500 text-sm">{fields.name.errors}</p>
+//               </div>
 
-              <div className="grid gap-2">
-                <Label>Subdirectory</Label>
-                <Input
-                  name={fields.subdirectory.name}
-                  key={fields.subdirectory.key}
-                  defaultValue={fields.subdirectory.initialValue}
-                  placeholder="Listing Name"
-                />
-                <p className="text-red-500 text-sm">
-                  {fields.subdirectory.errors}
-                </p>
-              </div>
+//               <div className="grid gap-2">
+//                 <Label>Subdirectory</Label>
+//                 <Input
+//                   name={fields.subdirectory.name}
+//                   key={fields.subdirectory.key}
+//                   defaultValue={fields.subdirectory.initialValue}
+//                   placeholder="Listing Name"
+//                 />
+//                 <p className="text-red-500 text-sm">
+//                   {fields.subdirectory.errors}
+//                 </p>
+//               </div>
 
-              <div className="grid gap-2">
-                <Label>Description</Label>
-                <Textarea
-                  name={fields.description.name}
-                  key={fields.description.key}
-                  defaultValue={fields.description.initialValue}
-                  placeholder="Brief description for your listing"
-                />
-                <p className="text-red-500 text-sm">
-                  {fields.description.errors}
-                </p>
-              </div>
-            </div>
-          </CardContent>
-          <CardFooter>
-            <SubmitButton text="Create Listing" />
-          </CardFooter>
-        </form>
-      </Card>
-    </div>
-  );
+//               <div className="grid gap-2">
+//                 <Label>Description</Label>
+//                 <Textarea
+//                   name={fields.description.name}
+//                   key={fields.description.key}
+//                   defaultValue={fields.description.initialValue}
+//                   placeholder="Brief description for your listing"
+//                 />
+//                 <p className="text-red-500 text-sm">
+//                   {fields.description.errors}
+//                 </p>
+//               </div>
+//             </div>
+//           </CardContent>
+//           <CardFooter>
+//             <SubmitButton text="Create Listing" />
+//           </CardFooter>
+//         </form>
+//       </Card>
+//     </div>
+//   );
 }
