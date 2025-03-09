d67fa4b41b1e9643d92847fb423ca60d7f8efb4f
Evan Hill <evanhill1989@gmail.com>
2025-03-09 10:00:38 -0400

    sellerOnboardingActions

--- app/onboarding/sellers/descriptions/page.tsx
+++ app/onboarding/sellers/descriptions/page.tsx
@@ -6,21 +6,33 @@ import {
 } from "@/components/ui/card";
 
 import DescriptionsForm from "@/components/onboarding/seller/forms/DescriptionsForm";
+import { requireUser } from "@/app/utils/auth";
+import { prisma } from "@/app/utils/db";
+import { redirect } from "next/navigation";
 
 export default async function DescriptionsPage() {
+  // Fetch user and their latest listing
+  const user = await requireUser();
+  const listing = await prisma.listing.findFirst({
+    where: { userId: user.id },
+  });
+
+  // Redirect if user doesn't have a listing or is on the wrong step
+  if (!listing || user.sellerOnboardingStep !== "descriptions") {
+    return redirect(`/onboarding/sellers/${user.sellerOnboardingStep}`);
+  }
+
   return (
-    <>
-      <Card className=" m-auto border-none shadow-none  lg:px-12 lg:w-2/3">
-        <CardHeader>
-          <CardTitle>Describe your business</CardTitle>
-          <CardDescription>
-            You can provide three different depths of descriptions. It is
-            advisable to fill out all three as best you can now, but you will
-            always be able to update this later.
-          </CardDescription>
-        </CardHeader>
-        <DescriptionsForm />
-      </Card>
-    </>
+    <Card className="m-auto border-none shadow-none lg:px-12 lg:w-2/3">
+      <CardHeader>
+        <CardTitle>Describe your business</CardTitle>
+        <CardDescription>
+          You can provide three different depths of descriptions. It is
+          advisable to fill out all three as best you can now, but you will
+          always be able to update this later.
+        </CardDescription>
+      </CardHeader>
+      <DescriptionsForm listing={listing} />
+    </Card>
   );
 }
